3618f081693ea82a0a206e6573ef67c0f81ca754
pmarcino <pat.marcino@gmail.com>
2017-02-05 20:44:52 -0600

    Add files via upload

---
+++ ArkCraft/Home.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<link rel="stylesheet" type="text/css" href="Style.css">
+	
+	</head>
+	<body>
+		<div id="header">
+			<h1 id="headerPageTitle">ArkCraft</h1>
+		</div>
+		<div id="body">
+			<div id="navBar">
+				<ul id="listNavBar">
+					<li><a href="Home.php" class="active">Home</a></li>
+					<li><a href="Projects.php">Projects</a></li>
+					<li><a href="Download.php">Download</a></li>
+					<li><a href="Contact.php">Contact</a></li>
+				</ul>
+			</div>
+			<div id="content">
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			<p>Some text..</p>
+			
+			</div>
+		</div>
+		<div id="footer">
+		
+		</div>
+	</body>
+</html>
